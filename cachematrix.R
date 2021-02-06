e3acda776b7921efde781afef743953fe7e20520
Gabriel Rivera Barreto <gabrielpaipa@gmail.com>
2021-02-06 14:26:36 -0600

    Based on makeVector, create makeCacheMatrix function with its setters and getters

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inv) m <<- inv
+    getInverse <- function() m
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
